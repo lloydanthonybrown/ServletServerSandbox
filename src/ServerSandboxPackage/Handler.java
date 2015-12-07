d7df25a9bc131f6a984365f60ab87eeb83aa9c18
lloydanthonybrown <lloydanthonybrown@gmail.com>
2015-12-06 22:13:02 -0700

    First commit to Server.

---
+++ src/ServerSandboxPackage/Handler.java
@@ -0,0 +1,7 @@
+package ServerSandboxPackage;
+
+import java.util.HashMap;
+
+public interface Handler {
+    public void handleIt(HashMap<String, Object> data);
+}
---
+++ src/ServerSandboxPackage/SpeakHandler.java
@@ -0,0 +1,19 @@
+package ServerSandboxPackage;
+
+import org.quickconnectfamily.json.JSONOutputStream;
+import java.util.HashMap;
+
+public class SpeakHandler implements Handler {
+    @Override
+    public void handleIt(HashMap<String, Object> dataMap) {
+        try{
+            JSONOutputStream outToClient = (JSONOutputStream)dataMap.remove("toClient");
+            System.out.println("Just got:" + dataMap + " from client");
+            dataMap.put("command", "Done");
+            outToClient.writeObject(dataMap);
+            System.out.println("just sent "+dataMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
